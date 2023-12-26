d115b81c08f4072c09d7eee8d3da56f0a7f68c9e
Hassan <hassanch5209@gmail.com>
2023-12-27 00:43:10 +0500

    added test scripts for user controllers

--- src/modules/user/controller/user.controller.spec.ts
+++ src/modules/user/controller/user.controller.spec.ts
@@ -1,18 +1,108 @@
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import User from '../entity/user.entity';
+import { UserService } from '../service/user.service';
 import { UserController } from './user.controller';
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    service = module.get<UserService>(UserService);
+    // repository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of users', async () => {
+      const mockUsers = { id: 2, email: 'test@test.com' };
+      jest.spyOn(service, 'findAll').mockResolvedValue([mockUsers]);
+
+      const result = await controller.findAll();
+
+      expect(result).toEqual([mockUsers]);
+    });
+
+    it('should return empty array if no user exists', async () => {
+      jest.spyOn(service, 'findAll').mockResolvedValue([]);
+
+      const result = await controller.findAll();
+
+      expect(result).toEqual([]);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a user by ID', async () => {
+      const mockUser = { id: 2, email: 'test@test.com' };
+      jest.spyOn(service, 'findOne').mockResolvedValue(mockUser);
+
+      const result = await controller.findOne('1');
+
+      expect(result).toEqual(mockUser);
+    });
+
+    it('should return undefined if user not found', async () => {
+      jest.spyOn(service, 'findOne').mockResolvedValue(undefined);
+      expect(await controller.findOne('1')).toEqual(undefined);
+    });
+  });
+
+  describe('create', () => {
+    it('should create a new user', async () => {
+      const mockUser = { id: 2, email: 'test@test.com' };
+      jest.spyOn(service, 'create').mockResolvedValue(mockUser);
+
+      const result = await controller.create(mockUser);
+
+      expect(result).toEqual(mockUser);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a user by ID', async () => {
+      const mockUser = { id: 2, email: 'test@test.com' };
+      jest.spyOn(service, 'update').mockResolvedValue(mockUser);
+      const result = await controller.update('2', mockUser);
+      expect(result).toEqual(mockUser);
+    });
+
+    it('should return undefined if user not found', async () => {
+      jest.spyOn(service, 'update').mockResolvedValue(undefined);
+      expect(await controller.update('1', { id: 2, email: 'test@test.com' })).toEqual(undefined);
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove a user by ID', async () => {
+      const mockUser = { id: 2, email: 'test@test.com' };
+      jest.spyOn(service, 'remove').mockResolvedValue(mockUser);
+
+      const result = await controller.remove('1');
+      expect(result).toEqual(mockUser);
+    });
+
+    it('should throw NotFoundException if user not found', async () => {
+      jest.spyOn(service, 'remove').mockRejectedValue(new NotFoundException);
+      await expect(controller.remove('1')).rejects.toThrow(NotFoundException);
+    });
   });
 });
--- src/modules/user/controller/user.controller.ts
+++ src/modules/user/controller/user.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import User from '../entity/user.entity';
+import { UserService } from '../service/user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) { }
+
+    @Get()
+    async findAll(): Promise<User[]> {
+        return this.userService.findAll();
+    }
+
+    @Get(':id')
+    async findOne(@Param('id') id: string): Promise<User | undefined> {
+        return this.userService.findOne(parseInt(id, 10));
+    }
+
+    @Post()
+    async create(@Body() user: Partial<User>): Promise<User> {
+        return this.userService.create(user);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() user: Partial<User>): Promise<User | undefined> {
+        return this.userService.update(parseInt(id, 10), user);
+    }
+
+    @Delete(':id')
+    async remove(@Param('id') id: string): Promise<User> {
+        return this.userService.remove(parseInt(id, 10));
+    }
+
+}
